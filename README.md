12afad03a85b215f2813030a949c779a24690e1f
Jose <sixgg4@gmail.com>
2018-11-14 22:37:03 -0500

    Minor changes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Web app that allows signed in users to plan and add. add or, remove their own scheduled trips and join other trips planned by other users.
 
+Tech – Python, Django, Bootstrap 4, SQLite, responsive
+
 Requirements.txt is available if needed.
--- apps/travel_buddy/views.py
+++ apps/travel_buddy/views.py
@@ -166,13 +166,7 @@ def all_json(request):
     return HttpResponse(serializers.serialize('json', my_trips), content_type='application/json')
 
 def create(request):
-    print("Creating Trip...")
-    print("*"*25)
-
-    print('Request: ', request)
-    print("*"*25)
-    print('Request POST: ', request.POST);
-    print("*"*25)
+    
     this_user_id = request.session['id']
     this_user = User.objects.get(id=this_user_id)
     print('User: ', this_user)
