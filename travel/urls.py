e230ba43372856169e727e6009571ac5299f538f
Jose <sixgg4@gmail.com>
2019-01-08 21:35:30 -0500

    revision from merge

--- travel/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.conf.urls import url, include
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    # url(r'^$', views.article_list, name='list'),
-    url(r'^', include('apps.travel_buddy.urls')),
-]
