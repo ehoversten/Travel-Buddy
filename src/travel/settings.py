a2364e898a21746e97a7d0d0612c301065f87d9f
Jose <sixgg4@gmail.com>
2019-01-08 21:28:43 -0500

    uh oh

--- src/travel/settings.py
+++ src/travel/settings.py
@@ -37,15 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD:src/travel/settings.py
 
     # installed application
     'apps.travel_buddy',
     'apps.accounts',
-=======
-    #added travel_buddy
-    'apps.travel_buddy',
->>>>>>> master:travel/settings.py
 ]
 
 MIDDLEWARE = [
