b5e6f7d3b5156dcaedfd4ebbb8e27ae505a43868
Jose <sixgg4@gmail.com>
2019-01-30 21:33:51 -0500

    testing timezone input

--- src/apps/destinations/forms.py
+++ src/apps/destinations/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.utils import timezone
-now = timezone.now()
+import datetime
+now = datetime.datetime.now()
+print(now)
+now = str(now)
+
+then = datetime.datetime.strptime(now, '%Y-%m-%d').date()
+
 
 
 class TripForm(forms.Form):
@@ -9,7 +14,7 @@ class TripForm(forms.Form):
         attrs={"class": "form-control", 'id': 'form_location', "placeholder": "Where are you traveling to?", }))
     description = forms.CharField(label="Description", widget=forms.Textarea(
         attrs={'class': 'form-control', 'id': 'form_description', 'rows':'3'}))
-    start_date = forms.DateTimeField(label='Start Date', widget=forms.DateInput(
+    start_date = forms.DateTimeField(label='Start Date', widget=forms.DateTimeInput(
         attrs={'type':'date','class': 'form-control', 'id': 'form_start_date'}))
     end_date = forms.DateTimeField(label="End Date", widget=forms.DateInput(
         attrs={'type':'date',"class": "form-control", 'id': 'form_end_date'}))
@@ -20,21 +25,23 @@ class TripForm(forms.Form):
         description = cleaned_data.get('description')
         start_date = cleaned_data.get('start_date')
         end_date = cleaned_data.get('end_date')
-        if not location:
-            msg = 'Please provide a location'
-            self.add_error('location', msg)
-        if not description:
-            msg = 'No description was provided'
-            self.add_error('description', msg)
-        if start_date < now:
-            msg = 'Start date must be in the future'
-            self.add_error('start_date', msg)
-        if end_date < now:
-            msg = 'End date must be in the future'
-            self.add_error('end_date', msg)
-        if end_date < start_date:
-            msg = 'End date must be after start date'
-            self.add_error('end_date', msg)
+        print(start_date)
+        print(now)
+        # if not location:
+        #     msg = 'Please provide a location'
+        #     self.add_error('location', msg)
+        # if not description:
+        #     msg = 'No description was provided'
+        #     self.add_error('description', msg)
+        # if start_date < now:
+        #     msg = 'Start date must be in the future'
+        #     self.add_error('start_date', msg)
+        # if end_date < now:
+        #     msg = 'End date must be in the future'
+        #     self.add_error('end_date', msg)
+        # if end_date < start_date:
+        #     msg = 'End date must be after start date'
+        #     self.add_error('end_date', msg)
         return cleaned_data
 
 
